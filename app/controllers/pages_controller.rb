921af33097c6cbca3773a66cfc38f9c2519d026c
haazime <h4zime@gmail.com>
2018-05-04 17:53:53 +0900

    user menu on nav

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,15 @@
 class PagesController < ApplicationController
+  helper_method :current_user
 
   def show
     render params[:id]
+  end
+
+  def current_user
+    OpenStruct.new(
+      name: '末吉 哉',
+      initials: 'HS',
+      hex_color: '#ffd7ad'
+    )
   end
 end
