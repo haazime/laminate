49218836a390367305bd1291aa1b912b11e6639c
haazime <h4zime@gmail.com>
2018-06-20 21:49:46 +0900

    imple

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,14 +2,22 @@ class PagesController < ApplicationController
   helper_method :current_user
 
   def show
-    render params[:id]
+    page_id = params[:id]
+    set_current_user(page_id)
+    render page_id
+  end
+
+  def set_current_user(page_id)
+    return if %w(sign_in).include?(page_id)
+    @current_user =
+      OpenStruct.new(
+        name: '末吉 哉',
+        initials: 'HS',
+        hex_color: '#ffd7ad'
+      )
   end
 
   def current_user
-    OpenStruct.new(
-      name: '末吉 哉',
-      initials: 'HS',
-      hex_color: '#ffd7ad'
-    )
+    @current_user
   end
 end
