add2e1daf903b7af7786088a918b08d75217d2fb
haazime <h4zime@gmail.com>
2018-07-01 11:46:59 +0900

    sign in

--- app/controllers/oauth_callbacks_controller.rb
+++ app/controllers/oauth_callbacks_controller.rb
@@ -1,14 +1,26 @@
 class OauthCallbacksController < ApplicationController
 
   def create
-    result = SignUpByOauthCommand.run(auth_hash)
-    if result.succeeded?
-      sign_in(result.user_id)
+    user_id = sign_up_or_find_user(auth_hash)
+    if user_id
+      sign_in(user_id)
       redirect_to root_url
     end
   end
 
   private
+
+    def sign_up_or_find_user(auth_hash)
+      user = Apps::User.find_by_oauth_account(auth_hash['provider'], auth_hash['uid'])
+      return user.id if user
+      sign_up(auth_hash)
+    end
+
+    def sign_up(auth_hash)
+      result = SignUpByOauthCommand.run(auth_hash)
+      return nil unless result.succeeded?
+      result.user_id
+    end
 
     def auth_hash
       request.env['omniauth.auth']
---
+++ spec/requests/sign_in_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'Sign in' do
+  context 'when signed up' do
+    it do
+      auth_hash = mock_auth_hash
+      SignUpByOauthCommand.run(auth_hash)
+      OmniAuth.config.add_mock(auth_hash['provider'].to_sym, auth_hash)
+
+      get '/auth/google_oauth2'
+      follow_redirect!
+      follow_redirect!
+
+      expect(response.body).to include(auth_hash['info']['name'])
+    end
+  end
+end
