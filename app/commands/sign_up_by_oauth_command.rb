b144dd717bff5ff10b4ec0be29925d15e6b31c39
haazime <h4zime@gmail.com>
2018-07-01 10:48:55 +0900

    refactor

--- app/commands/sign_up_by_oauth_command.rb
+++ app/commands/sign_up_by_oauth_command.rb
@@ -1,15 +1,17 @@
 class SignUpByOauthCommand < ApplicationCommand
 
   def run(auth_hash)
-    user =
-      Apps::User.new(
+    user = Apps::User.create_with_oauth_account!(
+      {
         name: auth_hash['info']['name'],
         email: auth_hash['info']['email'],
-        avatar_url: auth_hash['info']['image']
-      ) do |u|
-        u.build_oauth_account(provider: auth_hash['provider'], uid: auth_hash['uid'])
-      end
-    user.save!
+        avatar_url: auth_hash['info']['image'],
+      },
+      {
+        provider: auth_hash['provider'],
+        uid: auth_hash['uid']
+      }
+    )
   rescue
     failure
   else
--- app/models/apps/user.rb
+++ app/models/apps/user.rb
@@ -2,6 +2,13 @@ class Apps::User < ApplicationRecord
   has_one :oauth_account
 
   class << self
+    def create_with_oauth_account!(user_params, oauth_account_params)
+      new(user_params) do |u|
+        u.build_oauth_account(oauth_account_params)
+        u.save!
+      end
+    end
+
     def find_by_oauth_account(provider, uid)
       joins(:oauth_account).find_by(apps_oauth_accounts: { provider: provider, uid: uid })
     end
