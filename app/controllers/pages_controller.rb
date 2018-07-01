e3d97daafc7e1982d0934c702455f529daefaf9e
haazime <h4zime@gmail.com>
2018-07-01 20:46:08 +0900

    use production

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,23 +1,7 @@
 class PagesController < ApplicationController
-  helper_method :current_user
 
   def show
     page_id = params[:id]
-    set_current_user(page_id)
     render page_id
-  end
-
-  def set_current_user(page_id)
-    return if %w(sign_in).include?(page_id)
-    @current_user =
-      OpenStruct.new(
-        name: '末吉 哉',
-        initials: 'HS',
-        hex_color: '#ffd7ad'
-      )
-  end
-
-  def current_user
-    @current_user
   end
 end
