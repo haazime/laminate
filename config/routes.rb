e74a389a4d6dfd6feb79c847dee7768a4f9382af
Hajime Sueyoshi <h4zime@gmail.com>
2018-07-04 21:48:53 +0900

    impl (#38)

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'oauth_callbacks#create', as: :oauth_callback
   resource :session, only: [:new, :destroy]
 
-  resources :projects, only: [:index]
+  resources :projects, only: [:index, :new]
 
   #TODO: remove
   resources :pages
