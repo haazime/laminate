3230aeb20911ce8349a67e915f8a9a550889acd1
Hajime Sueyoshi <h4zime@gmail.com>
2018-07-05 21:10:07 +0900

    impl (#39)

---
+++ config/breadcrumbs.rb
@@ -0,0 +1,29 @@
+crumb :projects do
+  link t('projects.index.title'), projects_path
+end
+
+crumb :new_project do
+  link t('projects.new.title'), new_project_path
+  parent :projects
+end
+
+# crumb :project do |project|
+#   link project.name, project_path(project)
+#   parent :projects
+# end
+
+# crumb :project_issues do |project|
+#   link "Issues", project_issues_path(project)
+#   parent :project, project
+# end
+
+# crumb :issue do |issue|
+#   link issue.title, issue_path(issue)
+#   parent :project_issues, issue.project
+# end
+
+# If you want to split your breadcrumbs configuration over multiple files, you
+# can create a folder named `config/breadcrumbs` and put your configuration
+# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
+# folder are loaded and reloaded automatically when you change them, just like
+# this file (`config/breadcrumbs.rb`).
